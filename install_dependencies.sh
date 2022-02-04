b8de12c1b37921131d0236ec7b1ed86e06615b72
thecountrox <74064922+thecountrox@users.noreply.github.com>
2022-02-04 22:39:39 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "Enter your AUR Helper : (eg : yay paru etc.) (no pamac , fuck pamac)" aur
-sudo pacman -R libxft
+sudo pacman -Rdd libxft
 $aur -S libxft-bgra-git ttf-joypixels ttf-iosevka-nerd nitrogen maim flameshot dunst pamixer
