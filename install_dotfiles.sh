62f891c5ec0c88d7acef2f444a34a87042dcff10
thecountrox <prajoldavid2@gmail.com>
2022-03-08 18:11:18 +0530

    Update dotfile script to work (its much more bloated + a lot of useless things are done but eh who cares it works)

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,22 +4,23 @@ sudo mkdir -p /opt/
 cd /opt/
 sudo git clone https://github.com/thecountrox/dotfiles.git
 cd /opt/dotfiles/
-sudo mv * /opt/
 # picom and dunst config install and cleanup
 mkdir -p ~/.config/picom/
-sudo mv /opt/.config/picom/picom.conf ~/.config/picom/picom.conf
-sudo mv /opt/.config/dunst ~/.config/dunst 
-sudo rm -r /opt/.config/
+sudo mv /opt/dotfiles/.config/picom/picom.conf ~/.config/picom/picom.conf
+sudo mv /opt/dotfiles/.config/dunst ~/.config/dunst 
+sudo rm -r /opt/dofiles/.config/
 # install dwm dwmblocks dmenu st
-cd /opt/dwm/
+cd /opt/dotfiles/dwm/
 $install
-cd /opt/dwmblocks
+cd /opt/dotfiles/dwmblocks
 $install
-cd /opt/dmenu 
+cd /opt/dotfiles/dmenu 
 $install
-cd /opt/st
+cd /opt/dotfiles/st
 $install
-cd /opt/scripts
+cd /opt/dotfiles/scripts
 chmod +x *
 sudo mv * /usr/bin/
+cd /opt/dotfiles
+sudo mv * /opt/
 echo "Please look at the github for the required Dependencies that need to be installed for the functioning of certian things in the configs"
